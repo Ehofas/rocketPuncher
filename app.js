31761e71463e829987d8f3a63a79047bec6092ba
p998pmo <p998pmo@fspa.myntet.se>
2016-06-17 02:41:44 +0300

    Access control methods added

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(function(req,res,next){
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, TRACE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
