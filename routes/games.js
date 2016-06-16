dcdd1d0c95d2525c2c16e068d7a168a66c48f795
p998pmo <p998pmo@fspa.myntet.se>
2016-06-16 19:37:07 +0300

    Added validation

--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var dateFormat = require('dateformat');
 var router = express.Router();
+var ScoreCounter = require("./score-counter");
 
 /*
  * POST /games
@@ -22,23 +23,33 @@ var router = express.Router();
  *     "gameId": "5762a65f8802d648487b46ef"
  * }
  */
+
 router.post('/', function (req, res) {
     var db = req.db;
-
-    db.get('games').insert({
-        "status": "ACTIVE",
-        "teams": req.body.teams,
-        "endScore": req.body.endScore,
-        "deviceId": req.body.deviceId,
-        "startDate": dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss")
-    }, function (err, docsInserted) {
-        if (docsInserted) {
-            res.json({"gameId": docsInserted._id});
-        } else {
-            res.status(500).json({"message": "Database error", "error": "Database error"});
-        }
-    });
+    if(validateFields(res, req.body)) {
+        db.get('games').insert({
+            "status": "ACTIVE",
+            "teams": req.body.teams,
+            "endScore": req.body.endScore,
+            "deviceId": req.body.deviceId,
+            "startDate": dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss")
+        }, function (err, docsInserted) {
+            if (docsInserted) {
+                res.json({"gameId": docsInserted._id});
+            } else {
+                res.status(500).json({"message": "Database error", "error": "Database error"});
+            }
+        });
+    }
 });
+
+function validateFields(res, fields) {
+    if(fields.teams == undefined){
+        res.status(400).json({"message": "Field value invalid"});
+        return false;
+    }
+    return true;
+}
 
 /*
  * GET /games
@@ -60,9 +71,14 @@ router.get('/:id', function (req, res) {
 
     db.get('games').findOne({
         "_id": req.params.id
-    }, function (err, found) {
-        if (found) {
-            res.json(found);
+    }, function (err, game) {
+        if (game) {
+            db.get("scores").find({
+                "gameId": game._id
+            }, {}, function (e, scores) {
+                game.scores = ScoreCounter.countScores(game, scores);
+                res.json(game);
+            });
         } else {
             res.status(404).json({"message": "Game not found", "error": {}});
         }
